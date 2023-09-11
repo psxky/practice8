171ccda89aa5640c7f3f3927ab73d19d9fb768a3
psxky <zayczew.zaya@yandex.ru>
2023-09-11 23:25:55 +0300

    sorry :(

---
+++ yandexmaps/src/main/java/com/mirea/tarasov/yandexmaps/App.java
@@ -0,0 +1,14 @@
+package com.mirea.tarasov.yandexmaps;
+
+import android.app.Application;
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "8354c243-1105-4420-9785-e1b796e9c75f";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+// Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
